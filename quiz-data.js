8ee6d7c643f68a6be8994d36ce22fc9675fa8f6f
Ilya Trakhtenberg <ilya.trakhtenberg.01@gmail.com>
2024-03-13 19:36:42 +0200

    quiz data created

---
+++ quiz-data.js
@@ -0,0 +1,102 @@
+export const quizData = [
+  {
+    question: "У якім парку знаходзіцца Востраў Птушак?",
+    answers: [
+      { text: "Парк Перамогі", correct: true },
+      { trxt: "Парк Горкага", correct: false },
+      { text: "Лошыцкі парк", correct: false },
+      { text: "Парк 900-годдзя Менска", correct: false },
+      { text: "Парк Дразды", correct: false },
+    ],
+  },
+  {
+    question: "Што знаходзіцца ў канцы бульвара Шаўчэнкі?",
+    answers: [
+      { text: "Кінатэатр Кіеў", correct: true },
+      { text: "Стадыён Дынама", correct: false },
+      { text: "Донер Кінг", correct: false },
+      { text: "Завод Аліварыя", correct: false },
+      { text: "Помнік Шаўчэнку", correct: false },
+    ],
+  },
+  {
+    question: "Якая станцыя мэтро бліжэйшая да Камароўкі?",
+    answers: [
+      { text: "Плошча Якуба Коласа", correct: true },
+      { text: "Няміга", correct: false },
+      { text: "Парк Чалюскінцаў", correct: false },
+      { text: "Маладзёжная", correct: false },
+      { text: "Плошча Леніна", correct: false },
+    ],
+  },
+  {
+    question: "Што знаходзіцца за Чырвоным Касцёлам?",
+    answers: [
+      { text: "Менская фабрыка-кухня", correct: true },
+      { text: "Ліцэй БДУ", correct: false },
+      { text: "ГЦ Галерэя", correct: false },
+      { text: "Дом друка", correct: false },
+      { text: "Могілкі", correct: false },
+    ],
+  },
+  {
+    question: "Гэтая вуліца злучае Зялёны Луг з цэнтрам.",
+    answers: [
+      { text: "Лагойскі тракт", correct: true },
+      { text: "Прытыцкага", correct: false },
+      { text: "Ігуменскі тракт", correct: false },
+      { text: "Пр. Пераможцаў", correct: false },
+      { text: "Арлоўская", correct: false },
+    ],
+  },
+  {
+    question: "Гэтую статую можна знайсці каля пр. Дзяржынскага.",
+    answers: [
+      { text: "Зубр", correct: true },
+      { text: "Агурок", correct: false },
+      { text: "Францыск Скарына", correct: false },
+      { text: "Танк", correct: false },
+      { text: "Лідскі квас", correct: false },
+    ],
+  },
+  {
+    question: "Да гэтага раёна належыць батанічны сад",
+    answers: [
+      { text: "Першамайскі", correct: true },
+      { text: "Цэнтральны", correct: false },
+      { text: "Партызанскі", correct: false },
+      { text: "Ленінскі", correct: false },
+      { text: "Маскоўскі", correct: false },
+    ],
+  },
+  {
+    question: "Якая станцыя мэтро не належыць да сіняй галіны?",
+    answers: [
+      { text: "Купалаўская", correct: true },
+      { text: "Грушаўка", correct: false },
+      { text: "Акадэмія навук", correct: false },
+      { text: "Пятроўшчына", correct: false },
+      { text: "Плошча Перамогі", correct: false },
+    ],
+  },
+  {
+    question: "Колькі ў Менску будынкаў вышэйшах за 150м?",
+    answers: [
+      { text: "0", correct: true },
+      { text: "1", correct: false },
+      { text: "2", correct: false },
+      { text: "3", correct: false },
+      { text: "4", correct: false },
+    ],
+  },
+  {
+    question: "Якая вуліца не перасякае Свіслач?",
+    answers: [
+      { text: "Кальварыйская", correct: true },
+      { text: "Багдановіча", correct: false },
+      { text: "Пр. Машэрава", correct: false },
+      { text: "Першамайская", correct: false },
+      { text: "Дзянісаўская", correct: false },
+    ],
+  },
+];
