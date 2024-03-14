5487515ae5a132c16943c2929429c104f51e4001
Ilya Trakhtenberg <ilya.trakhtenberg.01@gmail.com>
2024-03-14 23:21:29 +0200

    working prototype

--- quiz-elements.js
+++ quiz-elements.js
@@ -1,3 +1,47 @@
-export const entering =
-  "<span class='entering'>Квіз: як добра ты ведаеш Менск?</span>";
-export const question = (question) => ``;
+export const prg = (text) => `<p>${text}</p>`;
+export const button = (name) => `<button>${name}</button>`;
+export const question = (question, selectedAnsw) =>
+  `<div>${prg(question.question)}
+  <form class='answers-form'>
+    ${question.answers
+      .map(
+        (answer, ind) =>
+          checkCol(selectedAnsw, answer, ind) + answerCol(answer, ind)
+      )
+      .join("")}
+  </form></div>`;
+export const result = (score, total, grades) =>
+  prg(score + "/" + total) + comm(score / total, grades);
+const answerCol = (answer, ind) =>
+  `<label class='answer' for='answ${ind}'>${answer.text}</label>`;
+const checkCol = (selectedAnsw, answer, ind) => `
+${
+  selectedAnsw
+    ? `${
+        selectedAnsw === `answ${ind}`
+          ? `<img alt='icon' src='./icons/${
+              answer.correct ? "correct" : "incorrect"
+            }.png'>`
+          : `${
+              answer.correct
+                ? `<img alt='icon' src='./icons/correct.png'>`
+                : "<br>"
+            }`
+      }`
+    : `<input type='radio' name='answer' id='answ${ind}' data-correct=${answer.correct}>`
+}`;
+const comm = (rate, grades) => {
+  let comm;
+  switch (parseInt(rate * 3)) {
+    case 0:
+      comm = grades.lo;
+      break;
+    case 1:
+      comm = grades.av;
+      break;
+    default:
+      comm = grades.hi;
+      break;
+  }
+  return prg(comm);
+};
--- quiz-script.js
+++ quiz-script.js
@@ -1,16 +1,42 @@
 import quizData from "./quiz-data.js";
-import { entering, question } from "./quiz-elements.js";
-const cntRef = document.querySelector(".content-area");
-const btnRef = document.querySelector(".button-area");
+import { prg, button, question, result } from "./quiz-elements.js";
+const mainRef = document.querySelector("main");
+const btnRef = () => document.querySelector("button");
+const answRefs = () => document.querySelectorAll("input");
 
-const quiz = () => {
-  cntRef.innerHTML = entering;
-  btnRef.innerHTML = "Start quiz";
-  btnRef.onclick = startQuiz;
+const renderContent = (elements) => {
+  mainRef.innerHTML = elements;
 };
 
-const startQuiz = () => {};
+const initialStage = () => {
+  renderContent(prg(quizData.legend) + button("Start quiz"));
+  btnRef().onclick = quizStage;
+};
 
-const finishQuiz = (crct) => {};
+const quizStage = () => {
+  let [num, score] = [0, 0];
+  const quiz = quizData.quiz;
+  const total = quizData.quiz.length;
+  const setQuestion = () => {
+    renderContent(question(quiz[num]), null);
+    answRefs().forEach(
+      (answ) =>
+        (answ.onchange = (e) => {
+          renderContent(
+            question(quiz[num], e.target.id) +
+              button(num < total - 1 ? "Next question" : "Finish quiz")
+          );
+          num++;
+          JSON.parse(e.target.dataset.correct) && score++;
+          btnRef().onclick = num < total ? setQuestion : setResult;
+        })
+    );
+  };
+  const setResult = () => {
+    renderContent(result(score, total, quizData.grades) + button("Try again"));
+    btnRef().onclick = initialStage;
+  };
+  setQuestion();
+};
 
-quiz();
+initialStage();
